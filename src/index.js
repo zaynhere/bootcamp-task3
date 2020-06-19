9656be1944b79003509c6c4f2da9a401c491f64b
zaynhere <zain.geneve@gmail.com>
2020-06-20 04:22:01 +0500

    bootcamp-session3-assignment

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'
 import Mediacard from './Mediacard'
 import './index.css';  
 
 
-ReactDOM.render(<Mediacard title="Getting Started With Props" body="Props are used to give the react components a dynamic value" imageUrl={<img src = "https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" />} />, document.querySelector('#root'));
+ReactDOM.render(<Mediacard title="Getting Started With Props" body="Props are used to give the react components a dynamic value" imageUrl={<img alt="img" src = "https://www.talkwalker.com/images/2020/blog-headers/image-analysis.png" />} />, document.querySelector('#root'));
